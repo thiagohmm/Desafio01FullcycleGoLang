89afa8c21bd648c1ef44f687c4de0e3fe2d19cdb
thiagohmm <thiagohmm@gmail.com>
2024-06-09 11:47:08 -0300

    add function client

---
+++ Client/go.mod
@@ -0,0 +1,5 @@
+module github.com/thiagohmm/Desafio01FullcycleGoLang
+
+go 1.22.3
+
+require github.com/valyala/fastjson v1.6.4
---
+++ Client/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/valyala/fastjson" // Replace "your-package-path" with the actual package path
+)
+
+func saveToFile(dolar string) error {
+	file, err := os.Create("dolar.txt")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = file.WriteString(dolar)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
+
+	if err != nil {
+		if ctx.Err() == context.DeadlineExceeded {
+			log.Println("timeout to write in database")
+		}
+		log.Fatal(err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var p fastjson.Parser
+	v, err := p.ParseBytes(body)
+	dolar := "Dolar " + v.Get("bid").String()
+	err = saveToFile(dolar)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Dolar", v.Get("bid").String())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
